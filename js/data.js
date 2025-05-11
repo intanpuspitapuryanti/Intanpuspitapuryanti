3c4b9f3f7374fe5a073b614bf60bed9e40b7d088
intanpuspitapuryanti <intanpuspita899@gmail.com>
2025-05-11 16:49:23 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,47 @@
+// Data untuk Game Tebak Gambar
+const tebakGambarData = [
+    {
+        image: 'assets/images/pisang.jpg',
+        correctAnswer: 'Pisang',
+        options: ['Pisang', 'Apel', 'Jeruk', 'Mangga']
+    },
+    {
+        image: 'assets/images/gajah.jpg',
+        correctAnswer: 'Gajah',
+        options: ['Gajah', 'Jerapah', 'Kuda', 'Singa']
+    },
+    {
+        image: 'assets/images/kucing.jpg',
+        correctAnswer: 'Kucing',
+        options: ['Kucing', 'Anjing', 'Kelinci', 'Harimau']
+    },
+    {
+        image: 'assets/images/mobil.jpg',
+        correctAnswer: 'Mobil',
+        options: ['Mobil', 'Sepeda', 'Pesawat', 'Kapal']
+    }
+];
+
+// Data untuk Game Tebak Suara
+const tebakSuaraData = [
+    {
+        sound: 'assets/sounds/suara_kucing.mp3',
+        correctAnswer: 'Kucing',
+        options: ['Kucing', 'Anjing', 'Singa', 'Harimau']
+    },
+    {
+        sound: 'assets/sounds/suara_ayam.mp3',
+        correctAnswer: 'Ayam',
+        options: ['Ayam', 'Burung', 'Bebek', 'Angsa']
+    },
+    {
+        sound: 'assets/sounds/suara_sapi.mp3',
+        correctAnswer: 'Sapi',
+        options: ['Sapi', 'Kambing', 'Kuda', 'Kerbau']
+    },
+    {
+        sound: 'assets/sounds/suara_gajah.mp3',
+        correctAnswer: 'Gajah',
+        options: ['Gajah', 'Jerapah', 'Badak', 'Harimau']
+    }
+];
